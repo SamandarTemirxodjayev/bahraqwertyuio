afdcadb14f72034ae45b87df7fe81ca32811293e
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-05-09 13:58:22 +0500

    add section 2 in indexpage

--- app.vue
+++ app.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <Head>
+      <Title>BAHRA</Title>
+    </Head>
     <NuxtLayout />
   </div>
 </template>
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.4s;
+  transition: all 0.3s;
 }
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
-  filter: blur(1rem);
+  filter: blur(4rem);
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,42 @@
   <div class="container mx-auto my-[3%]">
     <section class="justify-between flex">
       <div class="text-8xl font-semibold">
-        <p class="my-6">Tovuq </p>
-        <p class="my-6" style="color: #F9E01A;">go‘shtini</p>
+        <p class="my-6">Tovuq</p>
+        <p class="my-6" style="color: #f9e01a">go‘shtini</p>
         <p class="my-6">bizdan oling</p>
       </div>
-      <div class="items-center py-[3%]" >
-        <img src="/main.png" alt="">
+      <div class="items-center py-[3%]">
+        <img src="/main.png" alt="" />
+      </div>
+    </section>
+    <section
+      class="bg-no-repeat object-cover min-h-[810px] mt-[5%]"
+      style="background-image: url('/section2.png')"
+    >
+      <h1 class="ml-[20%] font-semibold text-5xl pt-[4%]">
+        Bizning afzalliklar
+      </h1>
+      <div class="ml-[20%] mt-16 font-medium flex flex-col">
+        <div class="my-10">
+          <h2 class="text-xl">Halollik</h2>
+          <p class="max-w-[250px]" style="color: #7e7e7e">
+            Bizda ishlab chiqarilgan mahsulotlar to‘laqonli ravishda qassoblar
+            tomonidan so‘yiladi
+          </p>
+        </div>
+        <div class="my-10">
+          <h2 class="text-xl">Tozalik</h2>
+          <p class="max-w-[250px]" style="color: #7e7e7e">
+            Har bir mahsulot alohida qadoqlanadi va upakovka qilinadi
+          </p>
+        </div>
+        <div class="my-10">
+          <h2 class="text-xl">Yetkazib berish xizmati</h2>
+          <p class="max-w-[250px]" style="color: #7e7e7e">
+            O‘zbekiston bo‘ylab istalgan joyga yetkazib berish xizmati mavjud.
+            Yetkazib berish mobaynida mahsulot sifati kafolatlanadi.
+          </p>
+        </div>
       </div>
     </section>
   </div>
