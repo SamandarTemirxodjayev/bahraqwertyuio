096f8835413c5d74d8986f6b112e136faae481c7
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-05-12 22:08:24 +0500

    fixed product page

--- components/ProductItem.vue
+++ components/ProductItem.vue
@@ -5,20 +5,35 @@
       <h2 class="text-lg font-light my-2">{{ product.name }}</h2>
       <p class="text-gray-700 font-bold text-xl">{{ product.price }}</p>
     </div>
+    <div @click="handle">
     <button
+      v-if="!handleclick"
       @click="add(product)"
       class="bg-yellow-300 font-bold p-3 rounded-2xl w-full my-2 flex items-center justify-center"
     >
       <div>Savatcha</div>
       <img src="/cart.png" alt="" class="ml-2" />
     </button>
+    <button
+      v-else
+      class="border border-green-500 font-bold p-3 rounded-2xl w-full my-2 flex items-center justify-center"
+    >
+      <div>Qo'shildi</div>
+    </button>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useCounterStore } from '~/store'
+import { ref } from 'vue'
 
 const counterStore = useCounterStore()
+
+const handleclick = ref(false)
+const handle = () => {
+  handleclick.value = true
+}
 
 const add = (product) => {
   counterStore.add(product)
