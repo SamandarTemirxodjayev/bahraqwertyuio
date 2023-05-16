b55a273280ba2c6fe166a62490a0f5aaf848e479
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-05-16 21:49:33 +0500

    add responsive to products use design

--- components/ProductItem.vue
+++ components/ProductItem.vue
@@ -1,46 +1,57 @@
 <template>
-  <div class="p-4 my-4 bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
-    <img :src="product.photo" alt="Product image" class="mx-auto block min-h-[130px]" />
+  <div
+    class="lg:p-4 p-2 lg:my-4 bg-white rounded-lg shadow-2xl overflow-hidden flex flex-col"
+  >
+    <img
+      :src="product.photo"
+      alt="Product image"
+      class="mx-auto block min-h-[130px]"
+    />
     <div class="lg:p-4 p-2">
       <h2 class="lg:text-lg text-sm font-light my-2">
-        {{ product.name.length > 15 ? product.name.substring(0, 15) + '...' : product.name }}
+        {{
+          product.name.length > 15
+            ? product.name.substring(0, 12) + "..."
+            : product.name
+        }}
       </h2>
-
-      <p class="text-gray-700 font-bold lg:text-xl text-sm">{{ product.price }}</p>
+      <p class="text-gray-700 font-bold lg:text-xl text-sm">
+        {{ product.price }}
+      </p>
     </div>
     <div @click="handle">
-    <button
-      v-if="!handleclick"
-      @click="add(product)"
-      class="bg-yellow-300 lg:font-bold font-semibold p-3 rounded-2xl w-full my-2 flex items-center justify-center"
-    >
-      <div>Savatcha</div>
-      <img src="/cart.png" alt="" class="ml-2" />
-    </button>
-    <button
-      v-else
-      class="border border-green-500 font-bold p-3 rounded-2xl w-full my-2 flex items-center justify-center"
-    >
-      <div>Qo'shildi</div>
-    </button>
+      <button
+        v-if="!handleclick"
+        @click="add(product)"
+        class="bg-yellow-300 lg:font-bold font-semibold p-3 rounded-2xl w-full lg:my-2 flex items-center justify-center"
+      >
+        <div>Savatcha</div>
+        <img src="/cart.png" alt="" class="ml-2" />
+      </button>
+      <button
+        v-else
+        class="border border-green-500 font-semibold p-3 rounded-2xl w-full lg:my-2 flex items-center justify-center"
+      >
+        <div>Qo'shildi</div>
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useCounterStore } from '~/store'
-import { ref } from 'vue'
+import { useCounterStore } from "~/store";
+import { ref } from "vue";
 
-const counterStore = useCounterStore()
+const counterStore = useCounterStore();
 
-const handleclick = ref(false)
+const handleclick = ref(false);
 const handle = () => {
-  handleclick.value = true
-}
+  handleclick.value = true;
+};
 
 const add = (product) => {
-  counterStore.add(product)
-}
+  counterStore.add(product);
+};
 </script>
 
 <script>
--- pages/products.vue
+++ pages/products.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-20 lg:my-0">
-    <h1 class="font-semibold text-5xl py-3 flex justify-center">Mahsulotlar</h1>
-    <div class="grid lg:grid-cols-5 gap-4 lg:mx-16 mx-4 md:grid-cols-3 grid-cols-1">
+    <h1 class="font-semibold lg:text-5xl text-3xl py-3 flex justify-center">Mahsulotlar</h1>
+    <div class="grid lg:grid-cols-5 gap-4 lg:mx-16 mx-4 md:grid-cols-3 grid-cols-2">
       <div v-for="item in products" :key="item">
         <ProductItem :product="item" />
       </div>
